af972082f57017897cbd06f8614ed3ffa92bd0c2
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2025-03-19 09:49:06 -0300

    refactor: improve responses and validations using exceptions

--- src/investments/investments.controller.ts
+++ src/investments/investments.controller.ts
@@ -3,42 +3,80 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  HttpStatus,
+  Put,
+  HttpCode,
 } from '@nestjs/common';
 import { InvestmentsService } from './investments.service';
-import { Prisma } from '@prisma/client';
+import { CreateInvestmentDto, UpdateInvestmentDto } from './investments.dto';
 
-@Controller('investments') // url path
+@Controller('investments')
 export class InvestmentsController {
   constructor(private readonly investmentsService: InvestmentsService) {}
 
   @Post()
-  create(@Body() createInvestmentDto: Prisma.InvestmentCreateInput) {
-    return this.investmentsService.create(createInvestmentDto);
+  async create(@Body() createInvestmentDto: CreateInvestmentDto) {
+    const investment =
+      await this.investmentsService.create(createInvestmentDto);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Investment created successfully',
+      data: investment,
+    };
   }
 
   @Get()
-  findAll() {
-    return this.investmentsService.findAll();
+  async findAll() {
+    const investments = await this.investmentsService.findAll();
+    if (!investments.length) {
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'No investments found',
+        data: [],
+      };
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Investments retrieved successfully',
+      data: investments,
+    };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.investmentsService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const investment = await this.investmentsService.findOne(id);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Investment retrieved successfully',
+      data: investment,
+    };
   }
 
-  @Patch(':id')
-  update(
+  @Put(':id')
+  async update(
     @Param('id') id: string,
-    @Body() updateInvestmentDto: Prisma.InvestmentUpdateInput,
+    @Body() updateInvestmentDto: UpdateInvestmentDto,
   ) {
-    return this.investmentsService.update(id, updateInvestmentDto);
+    const investment = await this.investmentsService.update(
+      id,
+      updateInvestmentDto,
+    );
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Investment updated successfully',
+      data: investment,
+    };
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.investmentsService.remove(id);
+  @HttpCode(HttpStatus.OK)
+  async remove(@Param('id') id: string) {
+    await this.investmentsService.remove(id);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Investment deleted successfully',
+    };
   }
 }
--- src/investments/investments.service.ts
+++ src/investments/investments.service.ts
@@ -1,36 +1,91 @@
-import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
-import { DatabaseService } from 'src/database/database.service';
+import { DatabaseService } from './../database/database.service';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateInvestmentDto, UpdateInvestmentDto } from './investments.dto';
 
 @Injectable()
 export class InvestmentsService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async create(createInvestmentDto: Prisma.InvestmentCreateInput) {
+  async create(createInvestmentDto: CreateInvestmentDto) {
+    if (new Date(createInvestmentDto.date) > new Date()) {
+      throw new HttpException(
+        'Date cannot be in the future',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const existingInvestment = await this.databaseService.investment.findFirst({
+      where: {
+        name: createInvestmentDto.name,
+        type: createInvestmentDto.type,
+      },
+    });
+
+    if (existingInvestment) {
+      throw new HttpException(
+        'Investment already exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
     return this.databaseService.investment.create({
+      omit: { id: true },
       data: createInvestmentDto,
     });
   }
 
   async findOne(id: string) {
-    return this.databaseService.investment.findUnique({
+    const investment = await this.databaseService.investment.findUnique({
+      omit: { id: true },
       where: { id },
     });
+
+    if (!investment) {
+      throw new HttpException('Investment not found', HttpStatus.NOT_FOUND);
+    }
+
+    return investment;
   }
 
   async findAll() {
-    return this.databaseService.investment.findMany({});
+    return this.databaseService.investment.findMany({
+      omit: { id: true },
+    });
   }
 
-  async update(id: string, updateInvestmentDto: Prisma.InvestmentUpdateInput) {
+  async update(id: string, updateInvestmentDto: UpdateInvestmentDto) {
+    if (new Date(updateInvestmentDto.date) > new Date()) {
+      throw new HttpException(
+        'Date cannot be in the future',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const investment = await this.databaseService.investment.findUnique({
+      where: { id },
+    });
+
+    if (!investment) {
+      throw new HttpException('Investment not found', HttpStatus.NOT_FOUND);
+    }
+
     return this.databaseService.investment.update({
+      omit: { id: true },
       where: { id },
       data: updateInvestmentDto,
     });
   }
 
   async remove(id: string) {
-    return this.databaseService.investment.delete({
+    const investment = await this.databaseService.investment.findUnique({
+      where: { id },
+    });
+
+    if (!investment) {
+      throw new HttpException('Investment not found', HttpStatus.NOT_FOUND);
+    }
+
+    await this.databaseService.investment.delete({
       where: { id },
     });
   }
